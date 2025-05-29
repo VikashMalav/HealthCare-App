91f4237216612b5d492e4710531d7460836df9ac
VikashMalav <malavvicky99@gmail.com>
2025-05-29 19:43:05 +0530

    bug fix again on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./App.css"
 import Header from './components/Header'
-import Sidebar from './components/sidebar'
+import Sidebar from './components/Sidebar'
 import DashboardMainContent from './components/dashboard/DashboardMainContent'
 import SimpleAppointmentCard from './components/SimpleAppointmentCard'
 
